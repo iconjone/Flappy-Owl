e0403a55008bbe43716140e1fa8ed7c0fcb51760
arvind-balaji <arvindb02@gmail.com>
2021-09-04 22:50:02 -0500

    Add socket.io server

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const router = express.Router();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, '/')));
-
-router.get('/', function(req, res){
-	res.sendFile(path.join(__dirname, '/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.use('/', router);
-app.listen(process.env.port || 3000);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
-console.log('Listening on port 3000');
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
